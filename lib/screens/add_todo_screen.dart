f7d3b88da23b8df27c84fc8192e16306add47568
Stanislav Petkov <stanislav.petkov@primeholding.com>
2023-10-19 10:51:14 +0300

    Add scaffold listener

--- lib/screens/add_todo_screen.dart
+++ lib/screens/add_todo_screen.dart
@@ -25,30 +25,39 @@ class AddTodoScreen extends StatelessWidget {
             );
           }
           if (state is TodosLoaded) {
-            return Card(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Column(
-                  children: [
-                    _inputField('ID', controllerId),
-                    _inputField('Task', controllerTask),
-                    _inputField('Description', controllerDescription),
-                    ElevatedButton(
-                      onPressed: () {
-                        var todo = Todo(
-                          id: controllerId.value.text,
-                          task: controllerTask.value.text,
-                          description: controllerDescription.value.text,
-                        );
-                        context.read<TodosBloc>().add(AddTodo(todo: todo));
-                        Navigator.pop(context);
-                      },
-                      style: ElevatedButton.styleFrom(
-                        primary: Theme.of(context).primaryColor,
+            return BlocListener<TodosBloc, TodosState>(
+              listener: (context, state) {
+                if (state is TodosLoaded) {
+                  ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                    content: Text('To do Added'),
+                  ));
+                }
+              },
+              child: Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    children: [
+                      _inputField('ID', controllerId),
+                      _inputField('Task', controllerTask),
+                      _inputField('Description', controllerDescription),
+                      ElevatedButton(
+                        onPressed: () {
+                          var todo = Todo(
+                            id: controllerId.value.text,
+                            task: controllerTask.value.text,
+                            description: controllerDescription.value.text,
+                          );
+                          context.read<TodosBloc>().add(AddTodo(todo: todo));
+                          Navigator.pop(context);
+                        },
+                        style: ElevatedButton.styleFrom(
+                          primary: Theme.of(context).primaryColor,
+                        ),
+                        child: const Text('Add To Do'),
                       ),
-                      child: const Text('Add To Do'),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             );
